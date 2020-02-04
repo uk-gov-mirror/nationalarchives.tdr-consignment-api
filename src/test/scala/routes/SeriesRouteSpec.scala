9c7fbf1595a7d7d25989f406aa6b5f5654f91efc
Sam Palmer <github@sampalmer.dev>
2020-02-04 13:45:02 +0000

    Add route tests

---
+++ src/test/scala/routes/SeriesRouteSpec.scala
@@ -0,0 +1,46 @@
+package routes
+
+import java.sql.PreparedStatement
+
+import akka.http.scaladsl.model.ContentTypes
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import db.repository.DbConnection
+import http.Routes.route
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import utils.TestUtils.validToken
+
+class SeriesRouteSpec extends AnyFlatSpec with Matchers with ScalatestRouteTest with BeforeAndAfterEach  {
+
+  override def beforeEach(): Unit = {
+    DbConnection.db.source.createConnection().prepareStatement("delete from consignmentapi.Series").executeUpdate()
+  }
+
+  "The api " should "return an empty series list" in {
+    val query: String = """{"query":"{getSeries{seriesid}}"}"""
+    Post("/graphql").withEntity(ContentTypes.`application/json`, query) ~> addCredentials(validToken) ~> route ~> check {
+      responseAs[String] shouldEqual """{"data":{"getSeries":[]}}"""
+    }
+  }
+
+  "The api " should "return the expected data" in {
+    val ps: PreparedStatement = DbConnection.db.source.createConnection().prepareStatement("insert into consignmentapi.Series (SeriesId) VALUES (1)")
+    ps.executeUpdate()
+    val query: String = """{"query":"{getSeries{seriesid}}"}"""
+    Post("/graphql").withEntity(ContentTypes.`application/json`, query) ~> addCredentials(validToken) ~> route ~> check {
+      responseAs[String] shouldEqual "{\"data\":{\"getSeries\":[{\"seriesid\":1}]}}"
+    }
+  }
+
+  "The api " should "return all requested fields" in {
+    val sql = "insert into consignmentapi.Series (SeriesId, BodyId, Name, Code, Description) VALUES (1,1,'Name','Code','Description')"
+    val ps: PreparedStatement = DbConnection.db.source.createConnection().prepareStatement(sql)
+    ps.executeUpdate()
+    val query: String = """{"query":"{getSeries{seriesid, bodyid, name, code, description}}"}"""
+    Post("/graphql").withEntity(ContentTypes.`application/json`, query) ~> addCredentials(validToken) ~> route ~> check {
+      val result = "{\"data\":{\"getSeries\":[{\"name\":\"Name\",\"description\":\"Description\",\"seriesid\":1,\"code\":\"Code\",\"bodyid\":1}]}}"
+      responseAs[String] shouldEqual result
+    }
+  }
+}
--- src/test/scala/utils/TestUtils.scala
+++ src/test/scala/utils/TestUtils.scala
@@ -1,5 +1,9 @@
 package utils
 
+import akka.http.scaladsl.model.headers.OAuth2BearerToken
+import com.tngtech.keycloakmock.api.KeycloakVerificationMock
+import com.tngtech.keycloakmock.api.TokenConfig.aTokenConfig
+
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 
@@ -9,5 +13,18 @@ object TestUtils {
       Await.result(future, timeout)
     }
   }
+  private val tdrPort: Int = 8000
+  private val testPort: Int = 8001
+  private val tdrMock: KeycloakVerificationMock = createServer("tdr", tdrPort)
+  private val testMock: KeycloakVerificationMock = createServer("test", testPort)
+
+  private def createServer(realm: String, port: Int) = {
+    val mock: KeycloakVerificationMock = new KeycloakVerificationMock(port, "tdr")
+    mock.start()
+    mock
+  }
+
+  def validToken: OAuth2BearerToken = OAuth2BearerToken(tdrMock.getAccessToken(aTokenConfig().build))
+  def invalidToken: OAuth2BearerToken = OAuth2BearerToken(testMock.getAccessToken(aTokenConfig().build))
 
 }
