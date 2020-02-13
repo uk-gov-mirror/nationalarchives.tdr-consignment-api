ed277b80326fadf9b6f0bf320b1e30021ecafc31
Sam Palmer <github@sampalmer.dev>
2020-02-13 14:49:04 +0000

    Update the Jenkinsfile

--- src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
@@ -2,7 +2,7 @@ package uk.gov.nationalarchives.tdr.api.http
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.{ContentType, ContentTypes, MediaType, StatusCodes}
 import akka.http.scaladsl.server.Directives.{as, authenticateOAuth2Async, complete, entity, get, getFromResource, path, post}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.RouteConcatenation._
@@ -45,6 +45,6 @@ object Routes {
     } ~ (get & path("healthcheck")) {
       complete(StatusCodes.OK)
     } ~ path("schema") {
-      getFromResource("schema.graphql")
+      getFromResource("schema.graphql", ContentTypes.`text/plain(UTF-8)`)
     }
 }
