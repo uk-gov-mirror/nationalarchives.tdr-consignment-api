a7ba4a387c583727ab980c49c90d16436daa7101
Sam Palmer <github@sampalmer.dev>
2020-02-17 14:10:37 +0000

    Removed the schema route

--- src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
@@ -2,8 +2,8 @@ package uk.gov.nationalarchives.tdr.api.http
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.{ContentType, ContentTypes, MediaType, StatusCodes}
-import akka.http.scaladsl.server.Directives.{as, authenticateOAuth2Async, complete, entity, get, getFromResource, path, post}
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives.{as, authenticateOAuth2Async, complete, entity, get, path, post}
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.RouteConcatenation._
 import akka.http.scaladsl.server.directives.Credentials
@@ -44,7 +44,5 @@ object Routes {
       }
     } ~ (get & path("healthcheck")) {
       complete(StatusCodes.OK)
-    } ~ path("schema") {
-      getFromResource("schema.graphql", ContentTypes.`text/plain(UTF-8)`)
     }
 }
