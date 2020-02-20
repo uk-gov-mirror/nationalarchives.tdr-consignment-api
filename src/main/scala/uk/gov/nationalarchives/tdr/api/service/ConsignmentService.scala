6bb669073d0083b337a3375d76349e1580eb506c
Sam Palmer <github@sampalmer.dev>
2020-02-20 15:00:59 +0000

    Add consignment service to context

--- src/main/scala/uk/gov/nationalarchives/tdr/api/service/ConsignmentService.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/service/ConsignmentService.scala
@@ -5,13 +5,13 @@ import java.time.Instant
 
 import uk.gov.nationalarchives.Tables.ConsignmentRow
 import uk.gov.nationalarchives.tdr.api.db.repository.ConsignmentRepository
-import uk.gov.nationalarchives.tdr.api.graphql.fields.ConsignmentFields.Consignment
+import uk.gov.nationalarchives.tdr.api.graphql.fields.ConsignmentFields.{AddConsignmentInput, Consignment}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class ConsignmentService(consignmentRepository: ConsignmentRepository)(implicit val executionContext: ExecutionContext) {
-    def addConsignment(consignment: Consignment): Future[Consignment] = {
-      val consignmentRow = ConsignmentRow(consignment.seriesid, consignment.userid, Timestamp.from(Instant.now()))
+    def addConsignment(addConsignmentInput: AddConsignmentInput): Future[Consignment] = {
+      val consignmentRow = ConsignmentRow(addConsignmentInput.seriesid, addConsignmentInput.userid, Timestamp.from(Instant.now()))
       consignmentRepository.addConsignment(consignmentRow).map(row => Consignment(row.consignmentid, row.seriesid, row.userid))
     }
 }
