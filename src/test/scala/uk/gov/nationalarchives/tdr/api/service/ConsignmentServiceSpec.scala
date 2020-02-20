f2d363b6c8c4d2db7ac3a61d548d9ce5be57d572
Sam Palmer <github@sampalmer.dev>
2020-02-20 13:25:54 +0000

    Add tests for the ConsignmentService
    There is only one test here as the arguments are guarded by sangria and
    so should never come through as null.

---
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/service/ConsignmentServiceSpec.scala
@@ -0,0 +1,31 @@
+package uk.gov.nationalarchives.tdr.api.service
+
+import java.sql.Timestamp
+import java.time.Instant
+
+import org.mockito.MockitoSugar
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import uk.gov.nationalarchives.Tables.ConsignmentRow
+import uk.gov.nationalarchives.tdr.api.db.repository.ConsignmentRepository
+import uk.gov.nationalarchives.tdr.api.graphql.fields.ConsignmentFields.Consignment
+import uk.gov.nationalarchives.tdr.api.utils.TestUtils._
+import org.mockito.ArgumentMatchers._
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class ConsignmentServiceSpec extends AnyFlatSpec with MockitoSugar with Matchers {
+  implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
+
+  "createConsignment" should "create a consignment given correct arguments" in {
+    val consignmentRepositoryMock = mock[ConsignmentRepository]
+    val mockResponse = Future.successful(ConsignmentRow(1L, 1L, Timestamp.from(Instant.now), Some(1)))
+    when(consignmentRepositoryMock.addConsignment(any[ConsignmentRow])).thenReturn(mockResponse)
+    val consignmentService = new ConsignmentService(consignmentRepositoryMock)
+    val result: Consignment = consignmentService.addConsignment(Consignment(Option.empty,1 ,1)).await()
+    result.seriesid shouldBe 1
+    result.userid shouldBe 1
+    result.consignmentid shouldBe defined
+    result.consignmentid.get shouldBe 1
+  }
+}
