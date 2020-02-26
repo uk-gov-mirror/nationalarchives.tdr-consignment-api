362efe9ee40b9a18a1b4451270d1f47a44732959
Sam Palmer <github@sampalmer.dev>
2020-02-26 12:07:18 +0000

    Move the UUID type to a different class

--- src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/ConsignmentFields.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/ConsignmentFields.scala
@@ -2,37 +2,14 @@ package uk.gov.nationalarchives.tdr.api.graphql.fields
 
 import java.util.UUID
 
-import sangria.marshalling.circe._
 import io.circe.generic.auto._
-import sangria.ast.StringValue
-import sangria.schema.ScalarType._
 import sangria.macros.derive._
-import sangria.schema.{Argument, Field, InputObjectType, ListType, ObjectType, ScalarType, fields}
-import sangria.validation.ValueCoercionViolation
+import sangria.marshalling.circe._
+import sangria.schema.{Argument, Field, InputObjectType, ObjectType, fields}
 import uk.gov.nationalarchives.tdr.api.graphql.ConsignmentApiContext
-
-import scala.util.{Failure, Success, Try}
+import uk.gov.nationalarchives.tdr.api.graphql.fields.FieldTypes._
 
 object ConsignmentFields {
-  private case object UuidCoercionViolation extends ValueCoercionViolation("Valid UUID expected")
-
-  private def parseUuid(s: String): Either[ValueCoercionViolation, UUID] = Try(UUID.fromString(s)) match {
-    case Success(uuid) => Right(uuid)
-    case Failure(_) => Left(UuidCoercionViolation)
-  }
-
-  implicit private val UuidType: ScalarType[UUID] = ScalarType[UUID]("UUID",
-    coerceOutput = (u, _) => u.toString,
-    coerceUserInput = {
-      case s: String => parseUuid(s)
-      case _ => Left(UuidCoercionViolation)
-    },
-    coerceInput = {
-      case StringValue(s, _, _, _, _) => parseUuid(s)
-      case _ => Left(UuidCoercionViolation)
-    }
-  )
-
   case class Consignment(consignmentid: Option[Long] = None, userid: UUID, seriesid: Long)
   case class AddConsignmentInput(seriesid: Long, userid: UUID)
 
