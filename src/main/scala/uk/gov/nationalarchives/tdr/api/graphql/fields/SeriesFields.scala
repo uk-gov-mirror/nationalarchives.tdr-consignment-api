dcd9dd0a143b72a77e9fc6644b8063a5d702ae4f
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-03-25 12:19:04 +0000

    Remove addSeries mutation
    This mutation is not used by any of the clients, and it depends on a tdr_admin user
    which needs to be thought through further before we add any admin functionality.

--- src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/SeriesFields.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/graphql/fields/SeriesFields.scala
@@ -1,22 +1,18 @@
 package uk.gov.nationalarchives.tdr.api.graphql.fields
 
-import sangria.marshalling.circe._
 import io.circe.generic.auto._
-import uk.gov.nationalarchives.tdr.api.graphql.ConsignmentApiContext
 import sangria.macros.derive._
-import sangria.schema.{Argument, Field, InputObjectType, ListType, ObjectType, OptionInputType, StringType, fields}
-import uk.gov.nationalarchives.tdr.api.graphql.Tags.{ValidateBody, ValidateIsAdmin}
+import sangria.marshalling.circe._
+import sangria.schema.{Argument, Field, ListType, ObjectType, StringType, fields}
+import uk.gov.nationalarchives.tdr.api.graphql.ConsignmentApiContext
+import uk.gov.nationalarchives.tdr.api.graphql.Tags.ValidateBody
 
 object SeriesFields {
   case class Series(seriesid: Long, bodyid: Long, name: Option[String] = None, code: Option[String] = None, description: Option[String] = None)
-  case class AddSeriesInput(bodyid: Long, name: Option[String] = None, code: Option[String] = None, description: Option[String] = None)
 
   implicit val SeriesType: ObjectType[Unit, Series] = deriveObjectType[Unit, Series]()
-  implicit val AddSeriesInputType: InputObjectType[AddSeriesInput] = deriveInputObjectType[AddSeriesInput]()
 
   val BodyArg = Argument("body", StringType)
-
-  private val SeriesInputArg = Argument("addSeriesInput", AddSeriesInputType)
 
   val queryFields: List[Field[ConsignmentApiContext, Unit]] = fields[ConsignmentApiContext, Unit](
     Field("getSeries", ListType(SeriesType),
@@ -24,14 +20,4 @@ object SeriesFields {
       resolve = ctx => ctx.ctx.seriesService.getSeries(ctx.arg(BodyArg)),
       tags=List(ValidateBody()))
   )
-
-  val mutationFields: List[Field[ConsignmentApiContext, Unit]] = fields[ConsignmentApiContext, Unit](
-    Field(
-      "addSeries",
-      SeriesType,
-      arguments = List(SeriesInputArg),
-      resolve = ctx => ctx.ctx.seriesService.addSeries(ctx.arg(SeriesInputArg)),
-      tags=List(ValidateIsAdmin())
-    )
-    )
 }
