45b885cf9f8ba28c5d80270429510652634fb8ce
sam <github@sampalmer.dev>
2020-04-01 17:39:23 +0100

    Update the service tests

--- src/test/scala/uk/gov/nationalarchives/tdr/api/service/SeriesServiceSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/service/SeriesServiceSpec.scala
@@ -1,5 +1,7 @@
 package uk.gov.nationalarchives.tdr.api.service
 
+import java.util.UUID
+
 import org.mockito.ArgumentMatchers.any
 import org.mockito.MockitoSugar
 import org.scalatest.flatspec.AnyFlatSpec
@@ -9,65 +11,67 @@ import uk.gov.nationalarchives.Tables.SeriesRow
 import uk.gov.nationalarchives.tdr.api.db.repository.SeriesRepository
 import uk.gov.nationalarchives.tdr.api.graphql.fields.SeriesFields
 import uk.gov.nationalarchives.tdr.api.graphql.fields.SeriesFields.AddSeriesInput
+import uk.gov.nationalarchives.tdr.api.utils.FixedUUIDSource
 import uk.gov.nationalarchives.tdr.api.utils.TestUtils._
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class SeriesServiceSpec extends AnyFlatSpec with MockitoSugar with Matchers {
   implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
+  val fixedUuidSource = new FixedUUIDSource()
 
   "getSeries" should "return all series if no argument is provided" in {
     val repoMock = setupSeriesResponses
-    val seriesService: SeriesService = new SeriesService(repoMock)
+    val seriesService: SeriesService = new SeriesService(repoMock, fixedUuidSource)
     val seriesResponse: Seq[SeriesFields.Series] = seriesService.getSeries(Option.empty).await()
 
     verify(repoMock, times(1)).getSeries()
     verify(repoMock, times(0)).getSeries(anyString())
     seriesResponse.length should equal(2)
-    checkFields(seriesResponse.head, SeriesCheck(1, 1, "name1", "code1", "description1"))
-    checkFields(seriesResponse.tail.head, SeriesCheck(2, 2, "name2", "code2", "description2"))
+    checkFields(seriesResponse.head, SeriesCheck(fixedUuidSource.uuid, fixedUuidSource.uuid, "name1", "code1", "description1"))
+    checkFields(seriesResponse.tail.head, SeriesCheck(fixedUuidSource.uuid, fixedUuidSource.uuid, "name2", "code2", "description2"))
   }
 
   "getSeries" should "return the specfic series for a body if one is provided" in {
     val repoMock = setupSeriesResponses
 
-    val seriesService: SeriesService = new SeriesService(repoMock)
+    val seriesService: SeriesService = new SeriesService(repoMock, fixedUuidSource)
     val seriesResponse: Seq[SeriesFields.Series] = seriesService.getSeries(Option("1")).await()
 
     verify(repoMock, times(0)).getSeries()
     verify(repoMock, times(1)).getSeries(anyString())
     seriesResponse.length should equal(1)
-    checkFields(seriesResponse.head, SeriesCheck(1, 1, "name1", "code1", "description1"))
+    checkFields(seriesResponse.head, SeriesCheck(fixedUuidSource.uuid, fixedUuidSource.uuid, "name1", "code1", "description1"))
   }
 
   "addSeries" should "insert series and return inserted series object" in {
-    val seriesId: Int = 123456789
-    val bodyId: Int = 987654321
+    val seriesId = UUID.randomUUID()
+    val bodyId = UUID.randomUUID()
     val seriesDescription: String = "Series Description"
     val seriesName: String = "Series Name"
     val seriesCode: String = "Series Code"
 
-    val mockSeriesRow = SeriesRow(bodyId, Option.apply(seriesCode), Option.apply(seriesName),
-      Option.apply(seriesDescription), Option.apply(seriesId))
+    val mockSeriesRow = SeriesRow(seriesId, bodyId, Option.apply(seriesCode), Option.apply(seriesName),
+      Option.apply(seriesDescription))
     val mockResponse: Future[SeriesRow] = Future.successful(mockSeriesRow)
 
     val repoMock = mock[SeriesRepository]
     when(repoMock.addSeries(any)).thenReturn(mockResponse)
 
-    val newSeriesInput = new AddSeriesInput(
+    val newSeriesInput = AddSeriesInput(
       bodyId,
       Option.apply(seriesCode),
       Option.apply(seriesName),
       Option.apply(seriesDescription))
 
-    val seriesService: SeriesService = new SeriesService(repoMock)
+    val seriesService: SeriesService = new SeriesService(repoMock, fixedUuidSource)
     val seriesResponse: SeriesFields.Series = seriesService.addSeries(newSeriesInput).await()
 
     verify(repoMock, times(1)).addSeries(any())
     checkFields(seriesResponse, SeriesCheck(seriesId, bodyId, seriesName, seriesCode, seriesDescription))
   }
 
-  case class SeriesCheck(seriesId: Int, bodyId: Int, name: String, code: String, description: String)
+  case class SeriesCheck(seriesId: UUID, bodyId: UUID, name: String, code: String, description: String)
 
   private def checkFields(series: SeriesFields.Series, seriesCheck: SeriesCheck) = {
     series.seriesid should equal(seriesCheck.seriesId)
@@ -78,8 +82,8 @@ class SeriesServiceSpec extends AnyFlatSpec with MockitoSugar with Matchers {
   }
 
   private def setupSeriesResponses = {
-    val seriesOne = SeriesRow(1, Option.apply("name1"), Option.apply("code1"), Option.apply("description1"), Some(1))
-    val seriesTwo = SeriesRow(2, Option.apply("name2"), Option.apply("code2"), Option.apply("description2"), Some(2))
+    val seriesOne = SeriesRow(fixedUuidSource.uuid, fixedUuidSource.uuid, Option.apply("name1"), Option.apply("code1"), Option.apply("description1"))
+    val seriesTwo = SeriesRow(fixedUuidSource.uuid, fixedUuidSource.uuid, Option.apply("name2"), Option.apply("code2"), Option.apply("description2"))
     val mockResponseAll: Future[Seq[SeriesRow]] = Future.successful(Seq(seriesOne, seriesTwo))
     val mockResponseOne: Future[Seq[SeriesRow]] = Future.successful(Seq(seriesOne))
 
--- src/test/scala/uk/gov/nationalarchives/tdr/api/service/TransferAgreementServiceSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/service/TransferAgreementServiceSpec.scala
@@ -1,5 +1,7 @@
 package uk.gov.nationalarchives.tdr.api.service
 
+import java.util.UUID
+
 import org.mockito.ArgumentMatchers._
 import org.mockito.MockitoSugar
 import org.scalatest.flatspec.AnyFlatSpec
@@ -7,35 +9,37 @@ import org.scalatest.matchers.should.Matchers
 import uk.gov.nationalarchives.Tables.TransferagreementRow
 import uk.gov.nationalarchives.tdr.api.db.repository.TransferAgreementRepository
 import uk.gov.nationalarchives.tdr.api.graphql.fields.TransferAgreementFields.{AddTransferAgreementInput, TransferAgreement}
+import uk.gov.nationalarchives.tdr.api.utils.FixedUUIDSource
 import uk.gov.nationalarchives.tdr.api.utils.TestUtils._
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class TransferAgreementServiceSpec extends AnyFlatSpec with MockitoSugar with Matchers {
   implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
+  val fixedUuidSource = new FixedUUIDSource()
 
   "addTransferAgreement" should "create a transfer agreement given correct arguments" in {
     val repositoryMock = mock[TransferAgreementRepository]
-    val mockResponse = Future.successful(TransferagreementRow(1L,
+    val transferAgreementUuid = UUID.randomUUID()
+    val consignmentUuid = UUID.randomUUID()
+    val mockResponse = Future.successful(TransferagreementRow(transferAgreementUuid,consignmentUuid,
       Some(true),
       Some(true),
       Some(true),
       Some(true),
       Some(true),
-      Some(true),
-      Some(1L)))
+      Some(true)))
 
     when(repositoryMock.addTransferAgreement(any[TransferagreementRow])).thenReturn(mockResponse)
-    val service = new TransferAgreementService(repositoryMock)
-    val result: TransferAgreement = service.addTransferAgreement(AddTransferAgreementInput(1L,
-      Some(true),
+    val service = new TransferAgreementService(repositoryMock, fixedUuidSource)
+    val result: TransferAgreement = service.addTransferAgreement(AddTransferAgreementInput(consignmentUuid,
       Some(true),
       Some(true),
       Some(true),
       Some(true),
       Some(true))).await()
 
-    result.consignmentId shouldBe 1
+    result.consignmentId shouldBe consignmentUuid
     result.allCrownCopyright.get shouldBe true
     result.allDigital.get shouldBe true
     result.allEnglish.get shouldBe true
