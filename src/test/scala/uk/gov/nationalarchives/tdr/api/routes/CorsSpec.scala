563380b9c31249f10ed022e1975a5a4541ba5082
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-04-29 16:58:02 +0100

    Add tests for Access-Control headers on cross-origin preflight requests

---
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/routes/CorsSpec.scala
@@ -0,0 +1,20 @@
+package uk.gov.nationalarchives.tdr.api.routes
+
+import akka.http.scaladsl.model.HttpMethods.{GET, OPTIONS, POST}
+import akka.http.scaladsl.model.headers._
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import uk.gov.nationalarchives.tdr.api.http.Routes.route
+
+class CorsSpec extends AnyFlatSpec with Matchers with ScalatestRouteTest {
+
+  "the pre-flight request" should "allow requests from the frontend" in {
+    Options("/graphql") ~> route ~> check {
+      header[`Access-Control-Allow-Origin`].map(_.value) should contain("http://localhost:9000")
+      header[`Access-Control-Allow-Credentials`].map(_.value) should contain("true")
+      header[`Access-Control-Allow-Headers`] should contain(`Access-Control-Allow-Headers`("Authorization", "Content-Type", "X-Requested-With"))
+      header[`Access-Control-Allow-Methods`] should contain(`Access-Control-Allow-Methods`(OPTIONS, POST, GET))
+    }
+  }
+}
