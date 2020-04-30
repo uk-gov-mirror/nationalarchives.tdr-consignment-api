4b0e1918505742bb46debd2a8c41df138cabc891
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2020-04-30 11:10:41 +0100

    Inject test config in Cors test
    This makes it easier to see where the test value comes from.

--- src/test/scala/uk/gov/nationalarchives/tdr/api/routes/CorsSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/routes/CorsSpec.scala
@@ -3,18 +3,23 @@ package uk.gov.nationalarchives.tdr.api.routes
 import akka.http.scaladsl.model.HttpMethods.{GET, OPTIONS, POST}
 import akka.http.scaladsl.model.headers._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import uk.gov.nationalarchives.tdr.api.http.Routes
 
 class CorsSpec extends AnyFlatSpec with Matchers with ScalatestRouteTest {
 
-  val route = new Routes(ConfigFactory.load()).route
+  private val baseConfig = ConfigFactory.load()
 
   "the pre-flight request" should "allow requests from the frontend" in {
+    val frontendUrl = "https://some-frontend.example.com"
+
+    val testConfig = baseConfig.withValue("frontend.url", ConfigValueFactory.fromAnyRef(frontendUrl))
+    val route = new Routes(testConfig).route
+
     Options("/graphql") ~> route ~> check {
-      header[`Access-Control-Allow-Origin`].map(_.value) should contain("https://tdr-frontend.example.com")
+      header[`Access-Control-Allow-Origin`].map(_.value) should contain(frontendUrl)
       header[`Access-Control-Allow-Credentials`].map(_.value) should contain("true")
       header[`Access-Control-Allow-Headers`] should contain(`Access-Control-Allow-Headers`("Authorization", "Content-Type", "X-Requested-With"))
       header[`Access-Control-Allow-Methods`] should contain(`Access-Control-Allow-Methods`(OPTIONS, POST, GET))
