f835f078869bbf127b19b37cdfed8be294fa4794
techncl <nigel_lutale@hotmail.co.uk>
2020-08-13 14:24:55 +0100

    Move private method to bottom of Class

--- src/main/scala/uk/gov/nationalarchives/tdr/api/service/ClientFileMetadataService.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/service/ClientFileMetadataService.scala
@@ -13,19 +13,6 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class ClientFileMetadataService(clientFileMetadataRepository: ClientFileMetadataRepository, uuidSource: UUIDSource)
                                (implicit val executionContext: ExecutionContext) {
-  private def rowToOutput(row: ClientfilemetadataRow): ClientFileMetadata = {
-    ClientFileMetadata(
-      row.fileid,
-      row.originalpath,
-      row.checksum,
-      row.checksumtype,
-      row.lastmodified.getTime,
-      row.createddate.getTime,
-      row.filesize,
-      row.datetime.getTime,
-      row.clientfilemetadataid
-    )
-  }
 
   def addClientFileMetadata(inputs: Seq[AddClientFileMetadataInput]): Future[Seq[ClientFileMetadata]] = {
     val rows: Seq[ClientfilemetadataRow] = inputs.map(i => ClientfilemetadataRow(
@@ -49,5 +36,19 @@ class ClientFileMetadataService(clientFileMetadataRepository: ClientFileMetadata
       case nse: NoSuchElementException => throw InputDataException(s"Could not find metadata for file $fileId", Some(nse))
       case e: SQLException => throw InputDataException(e.getLocalizedMessage, Some(e))
     }
+  }
+
+  private def rowToOutput(row: ClientfilemetadataRow): ClientFileMetadata = {
+    ClientFileMetadata(
+      row.fileid,
+      row.originalpath,
+      row.checksum,
+      row.checksumtype,
+      row.lastmodified.getTime,
+      row.createddate.getTime,
+      row.filesize,
+      row.datetime.getTime,
+      row.clientfilemetadataid
+    )
   }
 }
