e43a0b0a3cdc542c8f9c69bdcd89406ed1a07f2a
TomJKing <tom.king@nationalarchives.gov.uk>
2020-08-13 13:13:42 +0100

    Refactor to follow advanced directives example
    Changed logging function to follow more closely the akka documentation advanced directive example: https://doc.akka.io/docs/akka-http/current/routing-dsl/directives/debugging-directives/logRequestResult.html#building-advanced-directives

--- src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
@@ -1,11 +1,11 @@
 package uk.gov.nationalarchives.tdr.api.http
 
 import akka.actor.ActorSystem
-import akka.event.Logging
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.headers.Origin
 import akka.http.scaladsl.model.{HttpRequest, ResponseEntity, StatusCode, StatusCodes}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.RouteResult.{Complete, Rejected}
 import akka.http.scaladsl.server.directives.{Credentials, DebuggingDirectives, LoggingMagnet}
 import akka.http.scaladsl.server.{Directive0, Route, RouteResult}
 import akka.http.scaladsl.unmarshalling.Unmarshal
@@ -35,14 +35,16 @@ class Routes(val config: Config) extends Cors {
   }
 
   def logging: Directive0 = {
-    def logNon200Response(req: HttpRequest): RouteResult => Unit = {
-      case RouteResult.Complete(res) =>
-        if (isNon200Status(res.status)) {
-          val responseEntity = unmarshalEntity(res.entity)
-          logger.info(s"Non 200 Response: $req\nStatus Code: ${res.status}\nResponse Entity: $responseEntity")
-        }
-      case RouteResult.Rejected(rej) =>
-        logger.info(s"Rejected: " + rej.mkString(", "), Logging.InfoLevel)
+    def logNon200Response(req: HttpRequest)(res: RouteResult): Unit = {
+      res match {
+        case Complete(resp) =>
+          if (isNon200Status(resp.status)) {
+            val responseEntity = unmarshalEntity(resp.entity)
+            logger.info(s"Non 200 Response: $req\nStatus Code: ${resp.status}\nResponse Entity: $responseEntity")
+          }
+        case Rejected(reason) =>
+          logger.info(s"Rejected Reason: " + reason.mkString(", "))
+      }
     }
     DebuggingDirectives.logRequestResult(LoggingMagnet(_ => logNon200Response))
   }
