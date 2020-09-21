3e23eb15bbb4eb21557ba5e840c48223f6638c71
sam <github@sampalmer.dev>
2020-09-21 08:18:38 +0100

    Add the new column to the test data.

--- src/test/resources/scripts/init.sql
+++ src/test/resources/scripts/init.sql
@@ -52,6 +52,7 @@ CREATE TABLE IF NOT EXISTS File (
    FileId uuid NOT NULL DEFAULT '6e3b76c4-1745-4467-8ac5-b4dd736e1b3e',
    ConsignmentId uuid NOT NULL,
    UserId uuid DEFAULT NULL,
+   ChecksumMatches boolean,
    Datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
    PRIMARY KEY (FileId)
 );
