350236fc285e48eda2277e41fc5110a0dc518bba
sam <github@sampalmer.dev>
2020-09-30 11:39:04 +0100

    Remove commented code from the tests

--- src/test/scala/uk/gov/nationalarchives/tdr/api/service/FileMetadataServiceSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/service/FileMetadataServiceSpec.scala
@@ -169,16 +169,6 @@ class FileMetadataServiceSpec extends AnyFlatSpec with MockitoSugar with Matcher
     val filePropertyRepositoryMock = mock[FilePropertyRepository]
     val clientFileMetadataServiceMock = mock[ClientFileMetadataService]
     val fileId = UUID.randomUUID()
-//    val propertyId = UUID.randomUUID()
-//    val mockClientFileMetadata = ClientFileMetadata(fileId, Option.empty, Some("checksum"), Some("Mock"), 1, 1, Option.empty, 1, UUID.randomUUID)
-//    val mockFilePropertyRow = Some(FilepropertyRow(propertyId, Some("SomethingElse"), Some("Description"), Some("ShortName")))
-//    val mockMetadataResponse = Future.successful(
-//      FilemetadataRow(UUID.randomUUID(), fileId, propertyId, "value", Timestamp.from(FixedTimeSource.now), UUID.randomUUID())
-//    )
-//
-//    when(clientFileMetadataServiceMock.getClientFileMetadata(any[UUID])).thenReturn(Future(mockClientFileMetadata))
-//    when(filePropertyRepositoryMock.getPropertyByName(any[String])).thenReturn(Future(mockFilePropertyRow))
-//    when(fileMetadataRepositoryMock.addChecksumMetadata(any[FilemetadataRow], any[Option[Boolean]])).thenReturn(mockMetadataResponse)
 
     val service = new FileMetadataService(fileMetadataRepositoryMock, filePropertyRepositoryMock,
       clientFileMetadataServiceMock, FixedTimeSource, new FixedUUIDSource())
