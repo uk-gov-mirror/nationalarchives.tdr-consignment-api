14dd30f015d83163ae9d4c1fcb15e97e72376841
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2020-10-05 13:43:38 +0100

    Remove redundant test steps to keep test accurate to low-level actions

--- src/test/scala/uk/gov/nationalarchives/tdr/api/db/repository/ConsignmentRepositorySpec.scala
+++ src/test/scala/uk/gov/nationalarchives/tdr/api/db/repository/ConsignmentRepositorySpec.scala
@@ -1,13 +1,10 @@
 package uk.gov.nationalarchives.tdr.api.db.repository
 
-import java.sql.Timestamp
-import java.time.{Instant, LocalDateTime}
 import java.util.UUID
 
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import uk.gov.nationalarchives.Tables.ConsignmentRow
 import uk.gov.nationalarchives.tdr.api.db.DbConnection
 import uk.gov.nationalarchives.tdr.api.utils.TestUtils
 import uk.gov.nationalarchives.tdr.api.utils.TestUtils._
@@ -23,9 +20,6 @@ class ConsignmentRepositorySpec extends AnyFlatSpec with ScalaFutures with Match
     val consignmentId = UUID.fromString("0292019d-d112-465b-b31e-72dfb4d1254d")
 
     TestUtils.createConsignment(consignmentId, userId)
-    TestUtils.createFile(UUID.fromString("813231b3-2fbd-4869-abdf-0c42624c4d57"), consignmentId)
-    TestUtils.createFile(UUID.fromString("a235f269-026f-4fb2-a9de-966b49b2dba6"), consignmentId)
-    TestUtils.createFile(UUID.fromString("dac49dad-6e7e-4876-a9a6-0e151e13a136"), consignmentId)
 
     consignmentRepository.addParentFolder(consignmentId, Option("TEST PARENT FOLDER NAME")).futureValue
 
