a0b86b0b3b5dae667f0ed68af20f8a63b18d89d6
sam <sam.palmer@nationalarchives.gov.uk>
2020-11-16 14:47:00 +0000

    Add comment to validator method

--- src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/http/Routes.scala
@@ -29,6 +29,8 @@ class Routes(val config: Config) extends Cors {
   val ttlSeconds: Int = 10
   val url: String = config.getString("auth.url")
 
+  // We return None rather than a failed future because we're following the async authenticator docs
+  // https://doc.akka.io/docs/akka-http/10.0/routing-dsl/directives/security-directives/authenticateOAuth2Async.html
   def tokenAuthenticator(credentials: Credentials): Future[Option[Token]] = {
     credentials match {
       case Credentials.Provided(token) => Future {
