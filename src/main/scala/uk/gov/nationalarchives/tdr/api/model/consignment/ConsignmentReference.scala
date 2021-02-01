967d4016f8e72673ec065e0955f902636650b6bc
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2021-02-01 12:06:18 +0000

    Move error handling outwith method to avoid repetitive actions by method

--- src/main/scala/uk/gov/nationalarchives/tdr/api/model/consignment/ConsignmentReference.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/model/consignment/ConsignmentReference.scala
@@ -1,21 +1,19 @@
 package uk.gov.nationalarchives.tdr.api.model.consignment
 
-import java.io.IOException
-
 import uk.gov.nationalarchives.oci.Alphabet.Alphabet
 import uk.gov.nationalarchives.oci.{Alphabet, BaseCoder, IncludedAlphabet}
 
 object ConsignmentReference {
-  val alphabet: Either[IOException, Alphabet] = Alphabet.loadAlphabet(Right(IncludedAlphabet.GCRb25))
+  val alphabet: Alphabet = Alphabet.loadAlphabet(Right(IncludedAlphabet.GCRb25)) match {
+    case Left(error) => throw new Exception(s"Theres been an error creating the consignment reference", error)
+    case Right(alphabet) => alphabet
+  }
 
   def createConsignmentReference(transferStartYear: Int, consignmentSequenceId: Long): String = {
     val baseNumber = 25
-    val encoded = alphabet match {
-      case Left(error) => throw new Exception(s"Theres been an error creating the consignment reference", error)
-      case Right(alphabet) =>
-        val alphabetIndices = BaseCoder.encode(consignmentSequenceId, baseNumber)
-        Alphabet.toString(alphabet, alphabetIndices)
-    }
+    val alphabetIndices = BaseCoder.encode(consignmentSequenceId, baseNumber)
+    val encoded = Alphabet.toString(alphabet, alphabetIndices)
+
     s"TDR-$transferStartYear-$encoded"
   }
 }
