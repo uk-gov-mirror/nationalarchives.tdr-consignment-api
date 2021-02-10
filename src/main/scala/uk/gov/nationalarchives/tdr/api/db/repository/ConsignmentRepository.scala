d11cba3b15d6f35497ea8ff24fadf2cc861766fb
Lholbourn <loki.holbourn@nationalarchives.gov.uk>
2021-02-10 14:12:36 +0000

    Remove unused import

--- src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/ConsignmentRepository.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/ConsignmentRepository.scala
@@ -4,7 +4,6 @@ import java.sql.Timestamp
 import java.util.UUID
 
 import slick.jdbc.PostgresProfile.api._
-import slick.sql.SqlStreamingAction
 import uk.gov.nationalarchives.Tables.{Body, BodyRow, Consignment, ConsignmentRow, File, Series, SeriesRow}
 import uk.gov.nationalarchives.tdr.api.graphql.fields.ConsignmentFields
 import uk.gov.nationalarchives.tdr.api.service.TimeSource
