9b5c371ea3d1d63b7554e93f941958191ed14c98
Loki Holbourn <loki.holbourn@nationalarchives.gov.uk>
2021-03-30 14:11:39 +0100

    Refactor consignmentStatusService to get preferred data structure for current status

--- src/main/scala/uk/gov/nationalarchives/tdr/api/service/ConsignmentStatusService.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/service/ConsignmentStatusService.scala
@@ -1,14 +1,21 @@
 package uk.gov.nationalarchives.tdr.api.service
 
+import uk.gov
+import uk.gov.nationalarchives
+import uk.gov.nationalarchives.Tables
 import uk.gov.nationalarchives.Tables.ConsignmentstatusRow
 import uk.gov.nationalarchives.tdr.api.db.repository.ConsignmentStatusRepository
+import uk.gov.nationalarchives.tdr.api.graphql.fields.ConsignmentFields.CurrentStatus
 
 import java.util.UUID
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class ConsignmentStatusService(consignmentStatusRepository: ConsignmentStatusRepository) {
+class ConsignmentStatusService(consignmentStatusRepository: ConsignmentStatusRepository)
+                              (implicit val executionContext: ExecutionContext) {
 
-  def getConsignmentStatus(consignmentId: UUID): Future[Seq[ConsignmentstatusRow]] = {
-    consignmentStatusRepository.getConsignmentStatus(consignmentId)
+  def getConsignmentStatus(consignmentId: UUID): Future[CurrentStatus] = {
+    for {
+      upload <- consignmentStatusRepository.getConsignmentStatus(consignmentId).map(_.map(uploadRow => uploadRow.value).headOption)
+    } yield CurrentStatus(upload)
   }
 }
