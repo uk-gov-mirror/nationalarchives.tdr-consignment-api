608ce8f62e3c3b93b818cb614944e7e7b0af8700
ihoyle <ian.hoyle@nationalarchives.gov.uk>
2026-09-23 11:00:13 +0100

    improve update speed

--- src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/FileStatusRepository.scala
+++ src/main/scala/uk/gov/nationalarchives/tdr/api/db/repository/FileStatusRepository.scala
@@ -1,22 +1,31 @@
 package uk.gov.nationalarchives.tdr.api.db.repository
 
-import slick.jdbc.JdbcBackend
+import slick.jdbc.{JdbcBackend, RowsPerStatement}
 import slick.jdbc.PostgresProfile.api._
 import uk.gov.nationalarchives.Tables
 import uk.gov.nationalarchives.Tables.{File, Filestatus, FilestatusRow}
 import uk.gov.nationalarchives.tdr.api.graphql.fields.FileStatusFields.AddFileStatusInput
 
 import java.util.UUID
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class FileStatusRepository(db: JdbcBackend#Database) {
+class FileStatusRepository(db: JdbcBackend#Database)(implicit val executionContext: ExecutionContext) {
+  // Each row binds 3 parameters and Postgres allows a maximum of 65535 bind parameters per statement
+  private val maxRowsPerInsert = 5000
+
   private val insertQuery =
     Filestatus.map(t => (t.fileid, t.statustype, t.value)) returning Filestatus.map(r => (r.filestatusid, r.createddatetime)) into ((fileStatus, dbGeneratedValues) =>
       FilestatusRow(dbGeneratedValues._1, fileStatus._1, fileStatus._2, fileStatus._3, dbGeneratedValues._2)
     )
 
   def addFileStatuses(input: List[AddFileStatusInput]): Future[Seq[Tables.FilestatusRow]] = {
-    db.run(insertQuery ++= input.map(i => (i.fileId, i.statusType, i.statusValue)))
+    val inserts = input
+      .map(i => (i.fileId, i.statusType, i.statusValue))
+      .grouped(maxRowsPerInsert)
+      .map(batch => insertQuery.insertAll(batch, RowsPerStatement.All))
+      .toList
+
+    db.run(DBIO.sequence(inserts).map(_.flatten).transactionally)
   }
 
   def getFileStatus(consignmentId: UUID, statusTypes: Set[String], selectedFileIds: Option[Set[UUID]] = None): Future[Seq[FilestatusRow]] = {
